7d63a980487ae838b6932a0c01a38faaf0014f6a
coutoPL <coutoPL@gmail.com>
2023-01-17 22:26:22 +0100

    improvements

--- utils/collect-info-about-ror-es-kbn-with-hints.sh
+++ utils/collect-info-about-ror-es-kbn-with-hints.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
-echo "Preparing ES+KBN with ROR environment ..."
+echo "Preparing Elasticsearch & Kibana with ROR environment ..."
 
 determine_ror_es_dockerfile () {
   ROR_API_RESPONSE=$1
-  DEFAULT_ES_VERSION=$(echo "$ROR_API_RESPONSE" | jq .es[0] -cr)
-  ES_VERSIONS=$(echo "$ROR_API_RESPONSE" | jq .es -cr)
+  ES_VERSIONS_ARR=($(echo "$ROR_API_RESPONSE" | jq .[0] | jq 'to_entries | map(.value)' | jq .[].esVersions.es -cr | jq .[] -cr | uniq))
+  DEFAULT_ES_VERSION=$(echo ${ES_VERSIONS_ARR[0]})
+  ES_VERSIONS_STR=$(printf "%s," "${ES_VERSIONS_ARR[@]}")
 
-  read_es_version "$DEFAULT_ES_VERSION" "$ES_VERSIONS"
+  read_es_version "$DEFAULT_ES_VERSION" "$ES_VERSIONS_STR"
 
   while true; do
     read -p "Use ES ROR:
@@ -20,10 +21,11 @@ Your choice: " choice
       1 )
         export ES_DOCKERFILE="Dockerfile-use-ror-binaries-from-api"
 
-        DEFAULT_ES_ROR_VERSION=$(echo "$ROR_API_RESPONSE" | jq .pluginVersion -cr) # todo: fixme
-        ES_ROR_VERSIONS=$(echo "$ROR_API_RESPONSE" | jq .pluginVersion -cr)
+        ES_ROR_VERSIONS_ARR=($(echo "$ROR_API_RESPONSE" | jq .[0] | jq 'to_entries | map(.value)' | jq .[].pluginVersion -cr))
+        DEFAULT_ES_ROR_VERSION=$(echo ${ES_ROR_VERSIONS_ARR[0]})
+        ES_ROR_VERSIONS_STR=$(printf "%s," "${ES_ROR_VERSIONS_ARR[@]}")
 
-        read_ror_es_version "$DEFAULT_ES_ROR_VERSION" "$ES_ROR_VERSIONS"
+        read_ror_es_version "$DEFAULT_ES_ROR_VERSION" "$ES_ROR_VERSIONS_STR" "$ROR_API_RESPONSE"
         break
         ;;
       2 )
@@ -44,7 +46,7 @@ read_es_version () {
   AVAILABLE_ES_VERSIONS=$2
 
   while true; do
-    read -p "Enter ES version (default: $DEFAULT_ES_VERSION): " esVersion
+    read -p "Enter Elasticsearch version (default: $DEFAULT_ES_VERSION): " esVersion
     if [ -z "$esVersion" ]; then
       export ES_VERSION=$DEFAULT_ES_VERSION
       break
@@ -64,9 +66,10 @@ read_es_version () {
 read_ror_es_version () {
   DEFAULT_ES_ROR_VERSION=$1
   AVAILABLE_ES_ROR_VERSIONS=$2
+  ROR_API_RESPONSE=$3
 
   while true; do
-    read -p "Enter ES ROR version (default: $DEFAULT_ES_ROR_VERSION): " rorVersion
+    read -p "Enter ROR Elasticsearch version (default: $DEFAULT_ES_ROR_VERSION): " rorVersion
     if [ -z "$rorVersion" ]; then
       export ROR_ES_VERSION=$DEFAULT_ES_ROR_VERSION
       break
@@ -74,10 +77,19 @@ read_ror_es_version () {
 
     if [[ $AVAILABLE_ES_ROR_VERSIONS == *"$rorVersion"* ]];
     then
-      export ROR_ES_VERSION=$rorVersion
-      break
+      ES_VERSIONS_ARR=($(echo "$ROR_API_RESPONSE" | jq .[0] | jq '."'$rorVersion'".esVersions.es[]' -cr))
+      for i in "${ES_VERSIONS_ARR[@]}"
+      do
+        if [[ $i == "$ES_VERSION" ]]; then
+          export ROR_ES_VERSION=$rorVersion
+          break 2
+        fi
+      done
+
+      echo "ROR Elasticsearch $rorVersion is not available for Elasticsearch $ES_VERSION. Please try again ..."
+      continue
     else
-      echo "ES ROR $rorVersion is not available. Please try again ..."
+      echo "ROR Elasticsearch $rorVersion is not available. Please try again ..."
       continue
     fi
   done
@@ -85,7 +97,7 @@ read_ror_es_version () {
 
 read_es_ror_file_path () {
   while true; do
-    read -p "Enter ES ROR file path (it has to be placed in $(dirname "$0")): " path
+    read -p "Enter ROR Elasticsearch file path (it has to be placed in $(dirname "$0")): " path
     if [ -f "$path" ]; then
       export ES_ROR_FILE=$path
       break
@@ -99,10 +111,12 @@ read_es_ror_file_path () {
 determine_ror_kbn_dockerfile () {
   ROR_API_RESPONSE=$1
   PICKED_ES_VERSION=$2
-  DEFAULT_KBN_VERSION=$PICKED_ES_VERSION
-  KBN_VERSIONS=$(echo "$ROR_API_RESPONSE" | jq .kbn_universal -cr)
 
-  read_kbn_version "$DEFAULT_KBN_VERSION" "$KBN_VERSIONS"
+  KBN_VERSIONS_ARR=($(echo "$ROR_API_RESPONSE" | jq .[0] | jq 'to_entries | map(.value)' | jq .[].esVersions.kbn_universal -cr | jq .[] -cr | uniq))
+  DEFAULT_KBN_VERSION=$PICKED_ES_VERSION
+  KBN_VERSIONS_STR=$(printf "%s," "${KBN_VERSIONS_ARR[@]}")
+
+  read_kbn_version "$DEFAULT_KBN_VERSION" "$KBN_VERSIONS_STR"
 
   while true; do
     read -p "Use KBN ROR:
@@ -115,10 +129,11 @@ Your choice: " choice
       1 )
         export KBN_DOCKERFILE="Dockerfile-use-ror-binaries-from-api"
 
-        DEFAULT_KBN_ROR_VERSION=$(echo "$ROR_API_RESPONSE" | jq .pluginVersion -cr) # todo: fixme
-        KBN_ROR_VERSIONS=$(echo "$ROR_API_RESPONSE" | jq .pluginVersion -cr)
+        KBN_ROR_VERSIONS_ARR=($(echo "$ROR_API_RESPONSE" | jq .[0] | jq 'to_entries | map(.value)' | jq .[].pluginVersion -cr))
+        DEFAULT_KBN_ROR_VERSION=$(echo ${KBN_ROR_VERSIONS_ARR[0]})
+        KBN_ROR_VERSIONS_STR=$(printf "%s," "${KBN_ROR_VERSIONS_ARR[@]}")
 
-        read_ror_kbn_version "$DEFAULT_KBN_ROR_VERSION" "$KBN_ROR_VERSIONS"
+        read_ror_kbn_version "$DEFAULT_KBN_ROR_VERSION" "$KBN_ROR_VERSIONS_STR" "$ROR_API_RESPONSE"
         break
         ;;
       2 )
@@ -159,6 +174,7 @@ read_kbn_version () {
 read_ror_kbn_version () {
   DEFAULT_KBN_ROR_VERSION=$1
   AVAILABLE_KBN_ROR_VERSIONS=$2
+  ROR_API_RESPONSE=$3
 
   while true; do
     read -p "Enter ROR Kibana version (default: $DEFAULT_KBN_ROR_VERSION): " rorVersion
@@ -169,8 +185,17 @@ read_ror_kbn_version () {
 
     if [[ $AVAILABLE_KBN_ROR_VERSIONS == *"$rorVersion"* ]];
     then
-      export ROR_KBN_VERSION=$rorVersion
-      break
+      KBN_VERSIONS_ARR=($(echo "$ROR_API_RESPONSE" | jq .[0] | jq '."'$rorVersion'".esVersions.kbn_universal[]' -cr))
+      for i in "${KBN_VERSIONS_ARR[@]}"
+      do
+        if [[ $i == "$KBN_VERSION" ]]; then
+          export ROR_KBN_VERSION=$rorVersion
+          break 2
+        fi
+      done
+
+      echo "ROR Kibana $rorVersion is not available for Kibana $KBN_VERSION. Please try again ..."
+      continue
     else
       echo "ROR Kibana $rorVersion is not available. Please try again ..."
       continue
@@ -180,7 +205,7 @@ read_ror_kbn_version () {
 
 read_kbn_ror_file_path () {
   while true; do
-    read -p "Enter KBN ROR file path (it has to be placed in $(dirname "$0")): " path
+    read -p "Enter ROR Kibana file path (it has to be placed in $(dirname "$0")): " path
     if [ -f "$path" ]; then
       export KBN_ROR_FILE=$path
     else
@@ -191,8 +216,8 @@ read_kbn_ror_file_path () {
 }
 
 
-ROR_API_RESPONSE='{"es":["8.6.0"], "kbn_universal":["8.6.0"], "pluginVersion":"1.46.0"}'
-STATUS_CODE=$(curl -s -o /tmp/ror-api-response.txt -w "%{http_code}" https://api.beshu.tech/list_es_versions)
+ROR_API_RESPONSE='[{"1.46.0":{"pluginVersion":"1.46.0","esVersions":{"es":["8.6.0"], "kbn_universal":["8.6.0"], "pluginVersion":"1.46.0"}}},200]'
+STATUS_CODE=$(curl -s -o /tmp/ror-api-response.txt -w "%{http_code}" https://api.beshu.tech/list_es_versions/10)
 
 if [[ "$STATUS_CODE" -eq 200 ]] ; then
   ROR_API_RESPONSE=$(cat /tmp/ror-api-response.txt)

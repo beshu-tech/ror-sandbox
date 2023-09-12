4089b83a21a03d0ddd0f065a62d0223985be69f7
coutoPL <coutoPL@gmail.com>
2023-09-12 21:06:19 +0200

    older KBN versions support

--- ror-demo-cluster/images/kbn/install-ror-kbn-using-file.sh
+++ ror-demo-cluster/images/kbn/install-ror-kbn-using-file.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -e
 
+function verlte() {
+  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
+
+if [[ -z "$KBN_VERSION" ]]; then
+  echo "No KBN_VERSION variable is set"
+  exit 1
+fi
+
 echo "Installing KBN ROR from file..."
 /usr/share/kibana/bin/kibana-plugin install file:///tmp/ror.zip
-echo "Patching KBN ROR..."
-/usr/share/kibana/node/bin/node plugins/readonlyrestkbn/ror-tools.js patch
+if verlte "7.9.0" "$KBN_VERSION"; then
+  echo "Patching KBN ROR $ROR_VERSION..."
+  /usr/share/kibana/node/bin/node plugins/readonlyrestkbn/ror-tools.js patch
+fi
 echo "DONE!"

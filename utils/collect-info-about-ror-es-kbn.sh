3106bf80d6cb2f7268189b91bcb0aa7a7fa2e999
Simon Hearne <simon.hearne@gmail.com>
2023-11-02 13:15:02 +0000

    Update collect-info-about-ror-es-kbn.sh
    Fix typo `hitns` -> `hints`

--- utils/collect-info-about-ror-es-kbn.sh
+++ utils/collect-info-about-ror-es-kbn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 if ! command -v jq > /dev/null; then
-  source ../utils/collect-info-about-ror-es-kbn-without-hitns.sh
+  source ../utils/collect-info-about-ror-es-kbn-without-hints.sh
 else
   source ../utils/collect-info-about-ror-es-kbn-with-hints.sh
 fi

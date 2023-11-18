c46fe5892231c03003d0184c1ad4a24507e6d00c
coutoPL <coutoPL@gmail.com>
2023-11-18 21:34:40 +0100

    testing 8.10.4

--- utils/collect-info-about-ror-es-kbn.sh
+++ utils/collect-info-about-ror-es-kbn.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
 
-if ! command -v jq > /dev/null; then
-  source ../utils/collect-info-about-ror-es-kbn-without-hitns.sh
-else
-  source ../utils/collect-info-about-ror-es-kbn-with-hints.sh
-fi
+source ../utils/collect-info-about-ror-es-kbn-without-hints.sh

36b1243290eee9d3ecac18777c4e9d1f206bf50b
coutoPL <coutoPL@gmail.com>
2023-12-20 23:02:30 +0100

    wip

--- ror-demo-cluster/images/init-data/init-kbn.sh
+++ ror-demo-cluster/images/init-data/init-kbn.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
 import_saved_obj () {
     local USR=$1
     local PASS=$2
-    DEST_ID=$(curl -s -u "$USR":"$PASS" -XPOST -H "kbn-xsrf: true" "http://kbn-ror:5601/api/saved_objects/_import?createNewCopies=true" --form file=@export-ror.ndjson | jq -r .successResults[0].destinationId)
+    DEST_ID=$(curl -s -u "$USR":"$PASS" -XPOST -H "kbn-xsrf: true" "http://kbn-ror:5601/api/saved_objects/_import?createNewCopies=true" --form file=@export-ror-$USR.ndjson | jq -r .successResults[1].destinationId)
     echo "$DEST_ID"
 }
 
@@ -11,7 +11,7 @@ run_report () {
     local USR=$1
     local PASS=$2
     local DEST_ID=$3
-    curl -vk -u "$USR":"$PASS" -XPOST -H "kbn-xsrf: true" "http://kbn-ror:5601/api/reporting/generate/csv_searchsource?jobParams=%28browserTimezone%3AEurope%2FWarsaw%2Ccolumns%3A%21%28%29%2CobjectType%3Asearch%2CsearchSource%3A%28fields%3A%21%28%28field%3A%27%2A%27%2Cinclude_unmapped%3Atrue%29%29%2Cfilter%3A%21%28%29%2Cindex%3A%27$DEST_ID%27%2Cquery%3A%28language%3Akuery%2Cquery%3A%27%27%29%2Csort%3A%21%28%28_score%3Adesc%29%29%29%2Ctitle%3Asearch-example%2Cversion%3A%278.11.3%27%29"
+    curl -fvk -u "$USR":"$PASS" -XPOST -H "kbn-xsrf: true" "http://kbn-ror:5601/api/reporting/generate/csv_searchsource?jobParams=%28browserTimezone%3AEurope%2FWarsaw%2Ccolumns%3A%21%28%29%2CobjectType%3Asearch%2CsearchSource%3A%28fields%3A%21%28%28field%3A%27%2A%27%2Cinclude_unmapped%3Atrue%29%29%2Cfilter%3A%21%28%29%2Cindex%3A$DEST_ID%2Cquery%3A%28language%3Akuery%2Cquery%3A%27%27%29%2Csort%3A%21%28%28_score%3Adesc%29%29%29%2Ctitle%3Asearch-data-view-example-$USR%2Cversion%3A%278.11.3%27%29"
 }
 
 

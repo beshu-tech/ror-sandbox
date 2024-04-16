9baa8e07d979ebb7cf2d3d47a4dcb785149aa749
coutoPL <coutoPL@gmail.com>
2024-04-16 21:14:47 +0200

    reproduction

--- ror-demo-cluster/init-data.sh
+++ ror-demo-cluster/init-data.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
 for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-ldap-group1/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
+    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index1/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
 done
 
 for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-ldap-group2/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
+    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index2/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
 done
 
 for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-ldap-group3/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
-done
-
-for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-some-important-data/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
-done
-
-for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-ldap-group4/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
-done
-
-for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-another-important-data/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
-done
-
-for i in $(seq 0 10); do
-    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index-ldap-group5/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
+    curl -vk -u kibana:kibana -XPUT "http://localhost:19200/index3/_doc/$i" -H "Content-type: application/json" -d '{"df_country": "UK", "price": 100, "df_date": "2023-12-21T11:47:22.992000"}'
 done

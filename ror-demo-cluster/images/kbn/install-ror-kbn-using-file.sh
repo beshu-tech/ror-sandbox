fa5a463250f938666053e27278399b0d820297a4
Dawid Poliszak <dawid@readonlyrest.com>
2024-08-18 07:48:15 +0200

    Kibana 8.15-0 new node path support

--- ror-demo-cluster/images/kbn/install-ror-kbn-using-file.sh
+++ ror-demo-cluster/images/kbn/install-ror-kbn-using-file.sh
@@ -4,6 +4,10 @@ function verlte() {
   [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
 }
 
+function vergte() {
+    [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n 1)" ] || [ "$1" = "$2" ];
+};
+
 if [[ -z "$KBN_VERSION" ]]; then
   echo "No KBN_VERSION variable is set"
   exit 1
@@ -13,6 +17,11 @@ echo "Installing KBN ROR from file..."
 /usr/share/kibana/bin/kibana-plugin install file:///tmp/ror.zip
 if verlte "7.9.0" "$KBN_VERSION"; then
   echo "Patching KBN ROR $ROR_VERSION..."
-  /usr/share/kibana/node/bin/node plugins/readonlyrestkbn/ror-tools.js patch
+
+  if vergte "8.15.0" "$KBN_VERSION"; then
+  /usr/share/kibana/node/glibc-217/bin/node plugins/readonlyrestkbn/ror-tools.js patch;
+  else
+  /usr/share/kibana/node/bin/node plugins/readonlyrestkbn/ror-tools.js patch;
+  fi
 fi
 echo "DONE!"

e3ad2bcb22618cb0395cb7ded83e079c0afe8237
Mateusz Kołodziejczyk <coutoPL@gmail.com>
2024-11-13 21:11:32 +0100

    adaptations (#57)

--- ror-demo-cluster/images/es/install-ror-es-using-file.sh
+++ ror-demo-cluster/images/es/install-ror-es-using-file.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-function verlte() {
-  [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ]
+function greater_than_or_equal() {
+  [ "$1" = "$(echo -e "$1\n$2" | sort -V | tail -n 1)" ];
 }
+
 
 if [[ -z "$ES_VERSION" ]]; then
   echo "No ES_VERSION variable is set"
@@ -13,9 +14,9 @@ echo "Installing ES ROR from file..."
 /usr/share/elasticsearch/bin/elasticsearch-plugin install --batch file:///tmp/ror.zip
 
 echo "Patching ES ROR $ROR_VERSION..."
-if verlte "7.0.0" "$ES_VERSION"; then
+if greater_than_or_equal "$ES_VERSION" "7.0.0"; then
   /usr/share/elasticsearch/jdk/bin/java -jar /usr/share/elasticsearch/plugins/readonlyrest/ror-tools.jar patch
-elif verlte "6.7.0" "$ES_VERSION"; then
+elif greater_than_or_equal "$ES_VERSION" "6.7.0"; then
   "$JAVA_HOME"/bin/java -jar /usr/share/elasticsearch/plugins/readonlyrest/ror-tools.jar patch
 fi
 

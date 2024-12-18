9dd3f565959f10fb4bca80122d3b0fbbc9f63469
coutoPL <coutoPL@gmail.com>
2024-12-18 15:11:19 +0100

    wip

--- fleet-example/agent1/entrypoint.sh
+++ fleet-example/agent1/entrypoint.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
+POLICY_ID="elastic-policy"
 FLEET_ENROLLMENT_TOKEN=$(curl -k -s \
   -u elastic:elastic \
   https://kibana:5601/api/fleet/enrollment_api_keys | \
-  jq -r '.items[] | select(any(.; .policy_id == "elastic-policy")) | .api_key')
+  jq -r '.items[] | select(any(.; .policy_id == "'$POLICY_ID'")) | .api_key')
 
-if [[ -z "$ENROLLMENT_TOKEN" ]]; then
+if [[ -z "$FLEET_ENROLLMENT_TOKEN" ]]; then
   echo "Failed to retrieve enrollment token for policy_id: $POLICY_ID" >&2
   exit 1
 fi

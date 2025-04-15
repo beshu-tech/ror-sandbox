ed91727413dae320152a8c54b71a41a6f2be2bef
mgoworko <37329559+mgoworko@users.noreply.github.com>
2025-04-15 20:20:16 +0200

    [RORDEV-1436] Adjustments for a new required patch flag in ES ror-tools (#67)

--- ror-demo-cluster/images/es/install-ror-es-using-api.sh
+++ ror-demo-cluster/images/es/install-ror-es-using-api.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 function greater_than_or_equal() {
-  [ "$1" = "$(echo -e "$1\n$2" | sort -V | tail -n 1)" ];
+  # Strip the -pre part (or any suffix starting with -) from both versions
+  version_1=$(echo "$1" | sed 's/-pre.*//')
+  version_2=$(echo "$2" | sed 's/-pre.*//')
+  [ "$version_1" = "$(echo -e "$version_1\n$version_2" | sort -V | tail -n 1)" ];
 }
 
 if [[ -z "$ES_VERSION" ]]; then
@@ -9,8 +12,8 @@ if [[ -z "$ES_VERSION" ]]; then
   exit 1
 fi
 
-if [[ -z "$ROR_VERSION" ]]; then
-  echo "No $ROR_VERSION variable is set"
+if [[ ! -v ROR_VERSION || -z "$ROR_VERSION" ]]; then
+  echo "No ROR_VERSION variable is set"
   exit 2
 fi
 
@@ -18,10 +21,23 @@ echo "Installing ES ROR $ROR_VERSION..."
 /usr/share/elasticsearch/bin/elasticsearch-plugin install --batch "https://api.beshu.tech/download/es?esVersion=$ES_VERSION&pluginVersion=$ROR_VERSION&email=ror-sandbox%40readonlyrest.com"
 
 echo "Patching ES ROR $ROR_VERSION..."
+
+# Set Java path based on ES version
 if greater_than_or_equal "$ES_VERSION" "7.0.0"; then
-  /usr/share/elasticsearch/jdk/bin/java -jar /usr/share/elasticsearch/plugins/readonlyrest/ror-tools.jar patch
+  JAVA_BIN_PATH="/usr/share/elasticsearch/jdk/bin/java"
 elif greater_than_or_equal "$ES_VERSION" "6.7.0"; then
-  "$JAVA_HOME"/bin/java -jar /usr/share/elasticsearch/plugins/readonlyrest/ror-tools.jar patch
+  JAVA_BIN_PATH="$JAVA_HOME/bin/java"
+else
+  echo "Unsupported ES version: $ES_VERSION"
+  exit 1
 fi
 
+# Set OPTIONS based on ROR version
+if greater_than_or_equal "$ROR_VERSION" "1.64.0"; then
+  OPTIONS="--I_UNDERSTAND_AND_ACCEPT_ES_PATCHING=yes"
+else
+  OPTIONS=""
+fi
+
+$JAVA_BIN_PATH -jar /usr/share/elasticsearch/plugins/readonlyrest/ror-tools.jar patch $OPTIONS
 echo "DONE!"

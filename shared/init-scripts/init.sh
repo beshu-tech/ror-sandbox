88c4d2d2f2307fac66088d9293869eacbf7e969e
coutoPL <coutoPL@gmail.com>
2026-03-27 12:36:51 +0100

    wip

--- shared/init-scripts/init.sh
+++ shared/init-scripts/init.sh
@@ -5,5 +5,7 @@ cd "$(dirname "$0")"
 source utils/lib.sh
 
 createDataStream "logs-frontend-dev" && generate_log_documents 100 | putDocument "logs-frontend-dev"
-createIndex "logs-business-dev" && generate_log_documents 100 | putDocument "logs-business-dev"
+createDataStream "logs-business-dev" && generate_log_documents 100 | putDocument "logs-business-dev"
 createDataStream "logs-system-dev" && generate_log_documents 100 | putDocument "logs-system-dev"
+
+createIndex "data-business-index" && generate_log_documents 100 | putDocument "data-business-index"

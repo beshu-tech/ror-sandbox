3227742dc1dcc360ecb9d86d0572574578911668
coutoPL <coutoPL@gmail.com>
2026-07-08 09:59:01 +0200

    wip

--- shared/init-scripts/init.sh
+++ shared/init-scripts/init.sh
@@ -6,12 +6,18 @@ cd "$(dirname "$0")"
 
 source utils/lib.sh
 
-createDataStream "logs-frontend-dev" && generate_log_documents 100 | putDocument "logs-frontend-dev"
-createDataStream "logs-business-dev" && generate_log_documents 100 | putDocument "logs-business-dev"
-createDataStream "logs-system-dev" && generate_log_documents 100 | putDocument "logs-system-dev"
+#createDataStream "logs-frontend-dev" && generate_log_documents 100 | putDocument "logs-frontend-dev"
+#createDataStream "logs-business-dev" && generate_log_documents 100 | putDocument "logs-business-dev"
+#createDataStream "logs-system-dev" && generate_log_documents 100 | putDocument "logs-system-dev"
 
 #createKibanaDataView "logs-frontend-*" "Frontend logs" "@timestamp" "admin" "admin" "EndUsers"
 #createKibanaDataView "logs-business-*" "Business logs" "@timestamp" "admin" "admin" "BusinessUsers"
 #createKibanaDataView "logs-system-*" "System logs" "@timestamp" "admin" "admin" "Administrators"
 
-createIndex "data-business-index" && generate_log_documents 100 | putDocument "data-business-index"
+#createIndex "data-business-index" && generate_log_documents 100 | putDocument "data-business-index"
+
+createIndex "asdasd" && generate_log_documents 1 | putDocument "asdasd"
+createIndex "xasdasd" && generate_log_documents 1 | putDocument "xasdasd"
+createIndex "asdasdx" && generate_log_documents 1 | putDocument "asdasdx"
+createIndex "xasdasdx" && generate_log_documents 1 | putDocument "xasdasdx"
+createIndex "notmatching" && generate_log_documents 1 | putDocument "notmatching"
